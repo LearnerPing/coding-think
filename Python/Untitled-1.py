908fb3371a1976702a500d21ca102b5c7634cc2e
平 <932089165@qq.com>
2021-09-28 21:03:47 +0800

    阶段总结

--- Python/Untitled-1.py
+++ Python/Untitled-1.py
@@ -1,4 +1,10 @@
-def canWinNim(n) :
-    #  如果上来就踩到 4 的倍数，那就认输吧
-    #  否则，可以把对方控制在 4 的倍数，必胜
-    return n % 4 != 0
+print("输入第一个数")
+a = int(input())
+print("输入第二个数")
+b = int(input())
+#开始循环取余数,因为这里循环次数是未知的，所以我们使用while
+while a%b!=0:
+    num = a%b #交叉赋值
+    a = b
+    b = num #这段可以简写为 a,b=b,(a%b)
+print("最大公约数为%d" % b)
